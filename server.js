0fc41e12270bb2d82bc164a8aec29c3524e2bf37
sana-sboui <sbouisana1234@gmail.com>
2025-02-22 18:08:49 +0100

    connecting to db

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app=express()
+const PORT = process.env.PORT || 5000
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI).then(()=>console.log('Connected to DataBase')).catch(err=>console.log(err))
+
 app.use(express.json());
-app.listen(4000, () => {
+app.listen(PORT, () => {
     console.log('Server running on http://localhost:4000');
 });
 app.get('/', (req, res) => {
