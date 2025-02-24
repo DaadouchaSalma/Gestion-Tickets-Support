f0a1e394ce3c4f78b32561d3b7f08e26f9647d8e
zeinouba21 <benayedzeineb07@gmail.com>
2025-02-24 17:49:31 +0100

    admin ticket

--- models/ticket.js
+++ models/ticket.js
@@ -4,7 +4,7 @@ const ticketSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String},
   category: { type: String, required: true },
-  status: { type: String, enum: ["open", "in progress", "closed"], default: "open" },
+  status: { type: String, enum: ["A traiter","En attente", "En cours", "Résolu"], default: "A traiter" },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   agent: { type: mongoose.Schema.Types.ObjectId, ref: "Agent" },
   createdAt: { type: Date, default: Date.now },
