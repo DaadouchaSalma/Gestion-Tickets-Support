f8821a4df671ecce681e41af93109dcecd9c56d8
sana-sboui <sbouisana1234@gmail.com>
2025-03-08 15:59:31 +0100

    Sec

--- models/user.js
+++ models/user.js
@@ -4,8 +4,19 @@ const userSchema = new mongoose.Schema({
     nom: { type: String, required: true },
     prenom: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    mdp: { type: String, required: true },
-    role: { type: String, enum: ["admin", "agent", "simpleUser"], required: true }
+    password: { type: String, required: true },
+    role: { type: String, enum: ["admin", "agent", "simpleUser"], required: true, default: "simpleUser"}
 });
+
+userSchema.pre('save', async function (next) {
+    if(this.isModified('password')) {
+        this.password = await bcryptjs.hash(this.password,10)
+    }
+    next();
+})
+
+userSchema.methods.comparePassword = async function (userPassword) {
+    return bcryptjs.compare(userPassword, this.password);
+}
 
 module.exports = mongoose.model("User", userSchema);
